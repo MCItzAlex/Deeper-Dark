fbaa7336d712478c9dee4052e52395e396da9abc
Alex Balsillie <alex.balsillie@gmail.com>
2022-06-21 22:32:20 +0100

    added warden armor with placeholder textures

--- src/main/java/com/alex2k/deeperdark/item/ModItems.java
+++ src/main/java/com/alex2k/deeperdark/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.alex2k.deeperdark.item;
 
 import com.alex2k.deeperdark.DeeperDark;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,6 +21,23 @@ public class ModItems {
     // WARDEN HEART
     public static final RegistryObject<Item> WARDEN_HEART = ITEMS.register("warden_heart",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DEEPER_DARK_TAB).stacksTo(1).rarity(Rarity.EPIC)));
+
+    // WARDEN ARMOUR
+    public static final RegistryObject<Item> WARDEN_HELMET = ITEMS.register("warden_helmet",
+            () -> new ArmorItem(ModArmorMaterials.WARDEN, EquipmentSlot.HEAD,
+                new Item.Properties().tab(ModCreativeModeTab.DEEPER_DARK_TAB)));
+
+    public static final RegistryObject<Item> WARDEN_CHESTPLATE = ITEMS.register("warden_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.WARDEN, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModeTab.DEEPER_DARK_TAB)));
+
+    public static final RegistryObject<Item> WARDEN_LEGGINGS = ITEMS.register("warden_leggings",
+            () -> new ArmorItem(ModArmorMaterials.WARDEN, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModeTab.DEEPER_DARK_TAB)));
+
+    public static final RegistryObject<Item> WARDEN_BOOTS = ITEMS.register("warden_boots",
+            () -> new ArmorItem(ModArmorMaterials.WARDEN, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.DEEPER_DARK_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
